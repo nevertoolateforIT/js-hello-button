c610f4a2ce66c2f792a01eb266f79a3a0ea93a71
nevertoolateforITgit config --global user.name :nevertoolateforIT <nkandratenia@gmail.com>
2022-04-28 22:15:42 -0400

    first commit

---
+++ Week 13 JS/Calculator.js
@@ -0,0 +1,40 @@
+    //add function;
+    
+    function add(){
+
+    let first = prompt("Give first number");
+    let second = prompt("Give second number");
+    let result = +first + +second;
+    alert (`Answer: ${result}`);
+}
+
+    //subtract function;
+
+    function subtract(){
+        let first = prompt("Give first number");
+        let second = prompt("Give second number");
+        let result = +first - +second;
+        alert (`Answer: ${result}`);
+}
+
+    //multiply function;
+
+    function multiply(){
+
+        
+        let first = prompt("Give first number");
+        let second = prompt("Give second number");
+        let result = +first * +second;
+        alert (`Answer: ${result}`);  
+    }
+
+    //divide function;
+
+        function divide(){
+            let first = prompt("Give first number");
+            let second = prompt("Give second number");
+            let result = +first / +second;
+            alert (`Answer: ${result}`);   
+           
+        }
+         
